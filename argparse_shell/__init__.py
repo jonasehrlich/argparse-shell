c1d570970d340e9c540ac38015d0f67399e67dbe
Jonas Ehrlich <jonas.ehrlich@umsemi.com>
2024-04-09 13:41:18 +0200

    Switch to poetry

--- argparse_shell/__init__.py
+++ argparse_shell/__init__.py
@@ -1,6 +1,13 @@
-from . import version
-from .argparse_shell import ArgparseShell
-from .decorators import *
-from .namespace import *
+import importlib.metadata
 
-__version__ = version.version
+from argparse_shell.argparse_shell import ArgparseShell
+from argparse_shell.decorators import *
+from argparse_shell.namespace import *
+
+try:
+    # Poetry requires the version to be defined in pyproject.toml, load the version from the metadata,
+    # this is the recommended approach https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:  # pragma: no cover
+    # No metadata is available, this could be because the tool is running from source
+    __version__ = "unknown"
